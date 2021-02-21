9d5291bac85296b41292a4cfaf8cce4ad736bcf5
Joshua Serrano <josh@Joshuas-MacBook-Pro.local>
2021-02-20 23:40:34 -0800

    push milestone 3

--- local_test.py
+++ local_test.py
@@ -1,32 +1,58 @@
+from .smartthings import SmartThings
 import aiohttp
-import pysmartthings
-from pysmartthings import LocalControl
+from tinydb import TinyDB, Query
+from tinydb.operations import set
 import asyncio
-import sqlite3
 
-def getToken():
-    token_file = open('token.key','r')
-    token = token_file.read().strip()
-    token_file.close()
-    return token
+db = TinyDB('test.json')
+class LocalControl:
 
-async def intializeDB(token):
-    async with aiohttp.ClientSession() as session:
-        receive = await pysmartthings.LocalControl.intializeDevices(session, token)
+    async def intializeDevices(session,token):
+            api = SmartThings(session, token)
+            # clear state of db
+            db.truncate()
+            devices = await api.devices()
+            for device in devices:
+                if 'switch' in device.capabilities:
+                    await device.status.refresh()
+                    state = device.status.values['switch']
+                    db.insert({'device_id': device.device_id, 'name': device.name, 'label': device.label, 'capabilities': device.capabilities, 'state' : state})
+                else:
+                    db.insert({'device_id': device.device_id, 'name': device.name, 'label': device.label, 'capabilities': device.capabilities})
 
-def getDevice(deviceName):
-    device = LocalControl.getDevice(deviceName)
-    return device
+    def getDevice(deviceName):
+        deviceHold = Query()
+        result = db.search(deviceHold.name == deviceName)
+        deviceHold = LocalControl()
+        device = deviceHold.Device(result)
+        return device
 
-async def toggle(device, token):
-    async with aiohttp.ClientSession() as session:
-        await LocalControl.toggleDevice(device, token, session)
+    async def toggleDevice(deviceObject, token, session):
+        api = SmartThings(session, token)
+        deviceId = deviceObject.device_id
+        devices = await api.devices()
+        deviceHold = Query()
+        #db.search(deviceHold.device_id == deviceId)
+        for device in devices:
+            if device.device_id == deviceId:
+                if deviceObject.state == 'on':
+                    print('turning off')
+                    await device.command("main", "switch", "off")
+                    db.update({'state' : 'off'},deviceHold.device_id == deviceId )
+                if deviceObject.state == 'off':
+                    print('turning on')
+                    await device.command("main", "switch", "on")
+                    db.update({'state': 'on'}, deviceHold.device_id == deviceId)
 
-if __name__ == "__main__":
-    token = getToken()
-    #intialize DB
-    asyncio.run(intializeDB(token))
-    device = getDevice('test')
-    print(device[0]['state'])
-    asyncio.run(toggle(device,token))
+    class Device:
+        def __init__(self, device):
+            self.device_id = device[0]['device_id']
+            self.name = device[0]['name']
+            self.label = device[0]['label']
+            self.capabilities = device[0]['capabilities']
+            if device[0]['state']:
+                #handle device state
+                self.state = device[0]['state']
+            else:
+                self.state = "NULL"
 
--- pysmartthings/local.py
+++ pysmartthings/local.py
@@ -23,22 +23,36 @@ class LocalControl:
     def getDevice(deviceName):
         deviceHold = Query()
         result = db.search(deviceHold.name == deviceName)
-        return result
+        deviceHold = LocalControl()
+        device = deviceHold.Device(result)
+        return device
 
     async def toggleDevice(deviceObject, token, session):
         api = SmartThings(session, token)
-        deviceId = deviceObject[0]['device_id']
+        deviceId = deviceObject.device_id
         devices = await api.devices()
         deviceHold = Query()
         #db.search(deviceHold.device_id == deviceId)
         for device in devices:
             if device.device_id == deviceId:
-                if deviceObject[0]['state'] == 'on':
+                if deviceObject.state == 'on':
                     print('turning off')
                     await device.command("main", "switch", "off")
                     db.update({'state' : 'off'},deviceHold.device_id == deviceId )
-                if deviceObject[0]['state'] == 'off':
+                if deviceObject.state == 'off':
                     print('turning on')
                     await device.command("main", "switch", "on")
-                    db.update({ 'state' : 'on'},deviceHold.device_id == deviceId)
+                    db.update({'state': 'on'}, deviceHold.device_id == deviceId)
+
+    class Device:
+        def __init__(self, device):
+            self.device_id = device[0]['device_id']
+            self.name = device[0]['name']
+            self.label = device[0]['label']
+            self.capabilities = device[0]['capabilities']
+            if device[0]['state']:
+                #handle device state
+                self.state = device[0]['state']
+            else:
+                self.state = "NULL"
 
